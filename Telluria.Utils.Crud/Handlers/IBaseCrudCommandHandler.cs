666d81afd617702fa0918243b45414681085ce6a
Alexssander Leal Luz <aluz@telluria.com.br>
2021-11-24 16:03:13 -0300

    refactor(all): Remove unused functions reference update many registers

--- Telluria.Utils.Crud/Handlers/IBaseCrudCommandHandler.cs
+++ Telluria.Utils.Crud/Handlers/IBaseCrudCommandHandler.cs
@@ -4,18 +4,17 @@ using Telluria.Utils.Crud.Entities;
 
 namespace Telluria.Utils.Crud.Handlers
 {
-    public interface IBaseCrudCommandHandler<TEntity> :
-        ICommandHandler<BaseFindCommand<TEntity>, TEntity>,
-        IListCommandHandler<BaseListCommand<TEntity>, TEntity>,
-        IListCommandHandler<BaseListAllCommand<TEntity>, TEntity>,
-        ICommandHandler<BaseGetCommand<TEntity>, TEntity>,
-        ICommandHandler<BaseCreateCommand<TEntity>, TEntity>,
-        ICommandHandler<BaseCreateManyCommand<TEntity>, IEnumerable<TEntity>>,
-        ICommandHandler<BaseUpdateCommand<TEntity>>,
-        ICommandHandler<BaseUpdateManyCommand<TEntity>>,
-        ICommandHandler<BaseSoftDeleteCommand<TEntity>>,
-        ICommandHandler<BaseRemoveCommand<TEntity>>
-        where TEntity : BaseEntity
-    {
-    }
+  public interface IBaseCrudCommandHandler<TEntity> :
+    ICommandHandler<BaseFindCommand<TEntity>, TEntity>,
+    IListCommandHandler<BaseListCommand<TEntity>, TEntity>,
+    IListCommandHandler<BaseListAllCommand<TEntity>, TEntity>,
+    ICommandHandler<BaseGetCommand<TEntity>, TEntity>,
+    ICommandHandler<BaseCreateCommand<TEntity>, TEntity>,
+    ICommandHandler<BaseCreateManyCommand<TEntity>, IEnumerable<TEntity>>,
+    ICommandHandler<BaseUpdateCommand<TEntity>>,
+    ICommandHandler<BaseSoftDeleteCommand<TEntity>>,
+    ICommandHandler<BaseRemoveCommand<TEntity>>
+    where TEntity : BaseEntity
+  {
+  }
 }
