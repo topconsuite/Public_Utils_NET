6bdfe06b8685ea8cb9555ac5dc5bd8fb74265c1b
Alexssander Leal Luz <aluz@telluria.com.br>
2022-03-01 17:22:34 -0300

    feat(all): (Partial) Change Flunt to Fluent validation

--- Telluria.Utils.Crud/DTOs/BaseNotifiableRequestDTO.cs
+++ Telluria.Utils.Crud/DTOs/BaseNotifiableRequestDTO.cs
@@ -1,26 +1,26 @@
-﻿using Flunt.Notifications;
+﻿using System.Collections.Generic;
+using Flunt.Notifications;
 using Flunt.Validations;
-using System.Collections.Generic;
 using Telluria.Utils.Crud.Entities;
 
 namespace Telluria.Utils.Crud.DTOs
 {
-    public abstract class BaseNotifiableRequestDTO<TEntity> : Notifiable<Notification>, IRequestDTO<TEntity> where TEntity : BaseEntity
+  public abstract class BaseNotifiableRequestDTO<TEntity> : Notifiable<Notification>, IRequestDTO<TEntity> where TEntity : BaseEntity
+  {
+    protected Contract<IRequestDTO<TEntity>> _contract => new Contract<IRequestDTO<TEntity>>();
+
+    IEnumerable<Notification> IRequestDTO<TEntity>.Notifications => Notifications;
+
+    public new bool IsValid
     {
-        protected Contract<IRequestDTO<TEntity>> _contract => new Contract<IRequestDTO<TEntity>>();
-
-        IEnumerable<Notification> IRequestDTO<TEntity>.Notifications => Notifications;
-
-        public new bool IsValid
-        {
-            get
-            {
-                Clear();
-                Validate();
-                return base.IsValid;
-            }
-        }
-
-        public abstract void Validate();
+      get
+      {
+        Clear();
+        Validate();
+        return base.IsValid;
+      }
     }
+
+    public abstract void Validate();
+  }
 }
--- Telluria.Utils.Crud/DTOs/IRequestDTO.cs
+++ Telluria.Utils.Crud/DTOs/IRequestDTO.cs
@@ -1,13 +1,12 @@
-﻿using Flunt.Notifications;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Telluria.Utils.Crud.Entities;
 
 namespace Telluria.Utils.Crud.DTOs
 {
-    public interface IRequestDTO<TEntity> where TEntity : BaseEntity
-    {
-        void Validate();
-        IEnumerable<Notification> Notifications { get; }
-        bool IsValid { get; }
-    }
+  public interface IRequestDTO<TEntity> where TEntity : BaseEntity
+  {
+    void Validate();
+    List<FluentValidation.Results.ValidationFailure> Notifications { get; }
+    bool IsValid { get; }
+  }
 }
