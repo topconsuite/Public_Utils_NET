cac687a47271b14cd310d6b28b943aefa73d9a37
Alexssander Leal Luz <aluz@telluria.com.br>
2022-03-03 09:44:45 -0300

    refactor(all): add set in command result props

--- Telluria.Utils.Crud/CommandResults/ICommandResult.cs
+++ Telluria.Utils.Crud/CommandResults/ICommandResult.cs
@@ -17,15 +17,15 @@ namespace Telluria.Utils.Crud.CommandResults
 
   public interface ICommandResult
   {
-    CommandResultStatus Status { get; }
-    string Message { get; }
-    string ErrorCode { get; }
-    IEnumerable<FluentValidation.Results.ValidationFailure> Notifications { get; }
+    CommandResultStatus Status { get; set; }
+    string Message { get; set; }
+    string ErrorCode { get; set; }
+    IEnumerable<FluentValidation.Results.ValidationFailure> Notifications { get; set; }
   }
 
   public interface ICommandResult<TResult> : ICommandResult
   {
-    TResult Result { get; }
+    TResult Result { get; set; }
   }
 
   public interface IListCommandResult<TResult> : ICommandResult<IEnumerable<TResult>>
