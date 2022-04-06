c3cd45e4252399a78ee670f0f8e35de2ab75ed2b
Alexssander Leal Luz <aluz@telluria.com.br>
2022-04-06 15:01:57 -0300

    feat(all): add new optional parameter (includes)

--- Telluria.Utils.Crud/Commands/BaseCommands/BaseUpdateCommand.cs
+++ Telluria.Utils.Crud/Commands/BaseCommands/BaseUpdateCommand.cs
@@ -3,13 +3,15 @@ using Telluria.Utils.Crud.Entities;
 
 namespace Telluria.Utils.Crud.Commands.BaseCommands
 {
-    public class BaseUpdateCommand<TEntity> : ICommand where TEntity : BaseEntity
-    {
-        public TEntity Data { get; set; }
+  public class BaseUpdateCommand<TEntity> : ICommand where TEntity : BaseEntity
+  {
+    public TEntity Data { get; set; }
+    public string[] Includes { get; set; }
 
-        public BaseUpdateCommand(TEntity data)
-        {
-            Data = data;
-        }
+    public BaseUpdateCommand(TEntity data, params string[] includes)
+    {
+      Data = data;
+      Includes = includes;
     }
+  }
 }
