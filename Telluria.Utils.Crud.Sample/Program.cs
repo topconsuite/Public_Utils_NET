d467782131edb6cf478be22aeccaf244293c2be6
Bruno Gouvêa <bruno@telluria.com.br>
2022-06-17 18:02:14 -0300

    feat(all): graphql added to sample project

--- Telluria.Utils.Crud.Sample/Program.cs
+++ Telluria.Utils.Crud.Sample/Program.cs
@@ -1,5 +1,10 @@
+using GraphQL;
+using GraphQL.DataLoader;
+using GraphQL.Server;
+using GraphQL.Types;
 using Microsoft.EntityFrameworkCore;
 using Telluria.Utils.Crud.Sample;
+using GQLDI = GraphQL.MicrosoftDI;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,13 +21,28 @@ builder.Services.AddScoped<DbContext, AppDbContext>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<IProductCommandHandler, ProductCommandHandler>();
 
+// Add schema and register GraphQL
+builder.Services.AddSingleton<ISchema, GraphQLMainSchema>(services =>
+  new GraphQLMainSchema(new GQLDI.SelfActivatingServiceProvider(services)));
+
+GQLDI.GraphQLBuilderExtensions.AddGraphQL(builder.Services)
+  .AddServer(true)
+  .ConfigureExecution(options =>
+    options.EnableMetrics = true)
+  .AddSystemTextJson()
+  .AddDataLoader()
+  .AddGraphTypes(typeof(GraphQLMainSchema).Assembly);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
+
+  app.UseGraphQLGraphiQL();
+  app.UseGraphQLPlayground();
 }
 
 app.UseHttpsRedirection();
@@ -30,5 +50,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseGraphQL<ISchema>();
 
 app.Run();
