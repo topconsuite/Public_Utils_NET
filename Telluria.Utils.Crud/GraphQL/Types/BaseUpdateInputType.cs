ab6b6c0dccb814aa65d87996fef05c2fab40096a
Bruno Gouvêa <bruno@telluria.com.br>
2022-06-17 17:57:27 -0300

    feat(all): default contructor added do BaseUpdateInputType class

--- Telluria.Utils.Crud/GraphQL/Types/BaseUpdateInputType.cs
+++ Telluria.Utils.Crud/GraphQL/Types/BaseUpdateInputType.cs
@@ -7,6 +7,12 @@ namespace Telluria.Utils.Crud.GraphQL.Types
   public class BaseUpdateInputType<TEntity> : InputObjectGraphType<TEntity>
     where TEntity : BaseEntity
   {
+    public BaseUpdateInputType()
+    {
+      var entityName = typeof(TEntity).Name;
+      Field(x => x.Id, nullable: false).Description($"The id of the {entityName}");
+    }
+
     public override object ParseDictionary(IDictionary<string, object> value) => value;
   }
 }
