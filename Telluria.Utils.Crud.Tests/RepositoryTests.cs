a1424f7b1b66ef44783c23c8e01dd805a2bcf135
Bruno Gouvêa <bruno@telluria.com.br>
2022-06-29 20:09:53 -0300

    refactor(all): adjustments on repository tests

--- Telluria.Utils.Crud.Tests/RepositoryTests.cs
+++ Telluria.Utils.Crud.Tests/RepositoryTests.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Threading;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Microsoft.EntityFrameworkCore;
@@ -60,6 +61,7 @@ public class RepositoryTests
     var result = await _sut.FindAsync(tracking, filter, includeProperties, default);
 
     // Assert
+    result.Should().NotBeNull();
     result?.Id.Should().Be(entity!.Id);
   }
 
@@ -78,7 +80,7 @@ public class RepositoryTests
 
   [Theory]
   [MemberData(nameof(FindAsyncNotFoundTestData))]
-  public async Task FindAsync_ShouldReturnEntity_WhenFilterDoesNotHaveAMatch(
+  public async Task FindAsync_ShouldReturnNull_WhenFilterDoesNotHaveAMatch(
     bool tracking, Expression<Func<CustomEntity, bool>> filter, IEnumerable<string> includeProperties)
   {
     // Act
@@ -110,6 +112,7 @@ public class RepositoryTests
     var result = await _sut.GetAsync(entityId, tracking, includeProperties, default);
 
     // Assert
+    result.Should().NotBeNull();
     result?.Id.Should().Be(entityId);
   }
 
@@ -142,11 +145,15 @@ public class RepositoryTests
   public async Task ListAsync_ShouldReturnEntityList_WhenFilterHasAMatch(
     bool tracking, Expression<Func<CustomEntity, bool>> filter, IEnumerable<string> includeProperties)
   {
+    // Arrange
+    var expected = _entityMockList.AsQueryable().Where(filter).ToList();
+
     // Act
     var result = await _sut.ListAsync(tracking, filter, includeProperties, default);
 
     // Assert
     result.Should().NotBeNull();
+    result.Count().Should().Be(expected.Count);
   }
 
   [Theory]
@@ -154,14 +161,18 @@ public class RepositoryTests
   public async Task ListAllAsync_ShouldReturnEntityList_WhenFilterHasAMatch(
     bool tracking, Expression<Func<CustomEntity, bool>> filter, IEnumerable<string> includeProperties)
   {
+    // Arrange
+    var expected = _entityMockList.AsQueryable().Where(filter).ToList();
+
     // Act
     var result = await _sut.ListAllAsync(tracking, filter, includeProperties, default);
 
     // Assert
     result.Should().NotBeNull();
+    result.Count().Should().Be(expected.Count);
   }
 
-  private static IEnumerable<object[]> ListPagedAsyncSuccessTestData()
+  private static IEnumerable<object[]> ListAsyncSuccessTestData()
   {
     var first = _entityMockList.First();
     Expression<Func<CustomEntity, bool>> firstFilter = (t) => t.CreatedAt >= first.CreatedAt && t.Id != first.Id;
@@ -177,31 +188,45 @@ public class RepositoryTests
   }
 
   [Theory]
-  [MemberData(nameof(ListPagedAsyncSuccessTestData))]
-  public async Task ListPagedlAsync_ShouldReturnEntityList_WhenFilterHasAMatch(
+  [MemberData(nameof(ListAsyncSuccessTestData))]
+  public async Task ListAsync_ShouldReturnPagedEntityList_WhenFilterHasAMatch(
     uint page, uint perPage, bool tracking, Expression<Func<CustomEntity, bool>> filter, IEnumerable<string> includeProperties)
   {
+    // Arrange
+    var expected = _entityMockList.AsQueryable().Where(filter).ToList();
+
     // Act
     var result = await _sut.ListAsync(page, perPage, tracking, filter, includeProperties, default);
 
     // Assert
     result.Should().NotBeNull();
+    result.Page.Should().BeGreaterThan(0);
+    result.PerPage.Should().BeGreaterThan(0);
+    result.Records.Count().Should().BeLessThanOrEqualTo((int)result.PerPage);
+    result.TotalCount.Should().Be((ulong)expected.Count);
   }
 
   [Theory]
-  [MemberData(nameof(ListPagedAsyncSuccessTestData))]
-  public async Task ListAllPagedlAsync_ShouldReturnEntityList_WhenFilterHasAMatch(
+  [MemberData(nameof(ListAsyncSuccessTestData))]
+  public async Task ListAllAsync_ShouldReturnPagedEntityList_WhenFilterHasAMatch(
     uint page, uint perPage, bool tracking, Expression<Func<CustomEntity, bool>> filter, IEnumerable<string> includeProperties)
   {
+    // Arrange
+    var expected = _entityMockList.AsQueryable().Where(filter).ToList();
+
     // Act
     var result = await _sut.ListAllAsync(page, perPage, tracking, filter, includeProperties, default);
 
     // Assert
     result.Should().NotBeNull();
+    result.Page.Should().BeGreaterThan(0);
+    result.PerPage.Should().BeGreaterThan(0);
+    result.Records.Count().Should().BeLessThanOrEqualTo((int)result.PerPage);
+    result.TotalCount.Should().Be((ulong)expected.Count);
   }
 
   [Fact]
-  public async Task AddAsync_ShouldNotThrows_WhenSuccessAdded()
+  public async Task AddAsync_ShouldNotThrows_WhenSuccessfullyAdded()
   {
     // Arrange
     var entity = new CustomEntity();
@@ -214,7 +239,7 @@ public class RepositoryTests
   }
 
   [Fact]
-  public async Task UpdateAsync_ShouldNotThrows_WhenSuccessUpdated()
+  public async Task UpdateAsync_ShouldNotThrows_WhenSuccessfullyUpdated()
   {
     // Arrange
     var entity = _entityMockList.First();
@@ -227,7 +252,20 @@ public class RepositoryTests
   }
 
   [Fact]
-  public async Task SoftDeleteAsync_ShouldNotThrows_WhenSuccessDeleted()
+  public async Task UpdateAsync_ShouldThrows_WhenNotFound()
+  {
+    // Arrange
+    var entity = new CustomEntity();
+
+    // Act
+    var getResult = async () => await _sut.UpdateAsync(entity, default);
+
+    // Assert
+    await getResult.Should().ThrowAsync<Exception>().WithMessage($"*{entity.Id}*not found");
+  }
+
+  [Fact]
+  public async Task SoftDeleteAsync_ShouldNotThrows_WhenSuccessfullyDeleted()
   {
     // Arrange
     var entity = _entityMockList.First();
@@ -266,7 +304,7 @@ public class RepositoryTests
   }
 
   [Fact]
-  public async Task RemoveAsync_ShouldNotThrows_WhenSuccessRemoved()
+  public async Task RemoveAsync_ShouldNotThrows_WhenSuccessfullyRemoved()
   {
     // Arrange
     var entity = _entityMockList.First();
@@ -302,5 +340,30 @@ public class RepositoryTests
 
     // Assert
     await getResult.Should().ThrowAsync<Exception>().WithMessage($"*{Guid.Empty}*not found");
+  }
+
+  [Fact]
+  public async Task Commit_ShouldNotThrows_WhenSuccessfullySaveChanges()
+  {
+    // Act
+    var getResult = async () => await _sut.Commit(default);
+
+    // Assert
+    await getResult.Should().NotThrowAsync();
+  }
+
+  [Fact]
+  public async Task Commit_ShouldThrows_WhenUnableToSaveChanges()
+  {
+    // Arrange
+    var errorMessage = "something is wrong";
+    var cancellationToken = default(CancellationToken);
+    _dbContextMock.Setup(x => x.SaveChangesAsync(cancellationToken)).ThrowsAsync(new Exception(errorMessage));
+
+    // Act
+    var getResult = async () => await _sut.Commit(cancellationToken);
+
+    // Assert
+    await getResult.Should().ThrowAsync<Exception>().WithMessage(errorMessage);
   }
 }
