81ba063120c98874a32afdcb78cab57b07d1d07a
Alexssander Leal Luz <aluz@topconsuite.com>
2023-05-31 19:00:30 -0300

    feat(all): add new base commands classes for sorting and listing entities. - BaseListSortedCommand - BaseListAllSortedCommand

---
+++ Telluria.Utils.Crud/Commands/BaseCommands/BaseListAllSortedCommand.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Linq.Expressions;
+using System.Threading;
+using elluria.Utils.Crud.GraphQL.InputTypes;
+using Telluria.Utils.Crud.Entities;
+
+namespace Telluria.Utils.Crud.Commands.BaseCommands;
+
+public class BaseListAllSortedCommand<TEntity> : IListCommand
+  where TEntity : BaseEntity
+{
+  public BaseListAllSortedCommand(
+    uint page,
+    uint perPage,
+    Expression<Func<TEntity, bool>> where,
+    SortClauses sort,
+    string[] includes,
+    CancellationToken cancellationToken)
+  {
+    Page = page;
+    PerPage = perPage;
+    Where = where;
+    Sort = sort;
+    Includes = includes;
+    CancellationToken = cancellationToken;
+  }
+
+  public Expression<Func<TEntity, bool>> Where { get; set; }
+  public string[] Includes { get; set; }
+  public SortClauses Sort { get; set; }
+  public CancellationToken CancellationToken { get; set; }
+  public uint Page { get; set; }
+  public uint PerPage { get; set; }
+}
