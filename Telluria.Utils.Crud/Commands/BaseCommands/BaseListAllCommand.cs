69e010ec28a65d62a096b1ef587c828183e3fc0e
Alexssander Leal Luz <aluz@topconsuite.com>
2023-05-31 19:03:24 -0300

    refactor(all): code style adjustments

--- Telluria.Utils.Crud/Commands/BaseCommands/BaseListAllCommand.cs
+++ Telluria.Utils.Crud/Commands/BaseCommands/BaseListAllCommand.cs
@@ -3,31 +3,28 @@ using System.Linq.Expressions;
 using System.Threading;
 using Telluria.Utils.Crud.Entities;
 
-namespace Telluria.Utils.Crud.Commands.BaseCommands
+namespace Telluria.Utils.Crud.Commands.BaseCommands;
+
+public class BaseListAllCommand<TEntity> : IListCommand
+  where TEntity : BaseEntity
 {
-  public class BaseListAllCommand<TEntity> : IListCommand
-    where TEntity : BaseEntity
+  public BaseListAllCommand(
+    uint page,
+    uint perPage,
+    Expression<Func<TEntity, bool>> where,
+    string[] includes,
+    CancellationToken cancellationToken)
   {
-    public CancellationToken CancellationToken { get; set; }
-    public Expression<Func<TEntity, bool>> Where { get; set; }
-    public string[] Includes { get; set; }
-
-    public uint Page { get; set; }
-
-    public uint PerPage { get; set; }
-
-    public BaseListAllCommand(
-      uint page,
-      uint perPage,
-      Expression<Func<TEntity, bool>> where,
-      string[] includes,
-      CancellationToken cancellationToken)
-    {
-      Page = page;
-      PerPage = perPage;
-      Where = where;
-      Includes = includes;
-      CancellationToken = cancellationToken;
-    }
+    Page = page;
+    PerPage = perPage;
+    Where = where;
+    Includes = includes;
+    CancellationToken = cancellationToken;
   }
+
+  public Expression<Func<TEntity, bool>> Where { get; set; }
+  public string[] Includes { get; set; }
+  public CancellationToken CancellationToken { get; set; }
+  public uint Page { get; set; }
+  public uint PerPage { get; set; }
 }
