61b8d218e6f52c59f43b8187d34d3e200a263616
Alexssander Leal Luz <aluz@topconsuite.com>
2023-06-05 15:35:56 -0300

    refactor(all): add news queryable extension methods - OrderBy - ThenBy

--- Telluria.Utils.Crud/Helpers/QueryableExtensionHelper.cs
+++ Telluria.Utils.Crud/Helpers/QueryableExtensionHelper.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Linq;
 using System.Linq.Expressions;
 
@@ -6,17 +5,54 @@ namespace Telluria.Utils.Crud.Helpers;
 
 public static class QueryableExtension
 {
-  public static IQueryable<T> OrderByField<T>(this IQueryable<T> q, string sortColumn, bool asc)
+  /// <summary>
+  ///   OrderBy extension method for IQueryable.
+  /// </summary>
+  /// <param name="source"> IQueryable. </param>
+  /// <param name="propertyName"> Property name. </param>
+  /// <param name="descending"> Boolean indicating descending. </param>
+  /// <typeparam name="T"> Type of IQueryable. </typeparam>
+  /// <returns> IOrderedQueryable (ORDER BY). </returns>
+  public static IOrderedQueryable<T> OrderBy<T>(this IQueryable<T> source, string propertyName, bool descending = false)
   {
     var param = Expression.Parameter(typeof(T), "p");
-    var prop = Expression.Property(param, sortColumn);
-    var exp = Expression.Lambda(prop, param);
-    var method = asc ? "OrderBy" : "OrderByDescending";
+    var prop = Expression.Property(param, propertyName);
+    var lambda = Expression.Lambda(prop, param);
 
-    Type[] types = { q.ElementType, exp.Body.Type };
+    var method = descending ? "OrderByDescending" : "OrderBy";
+    var orderByCall = Expression.Call(
+      typeof(Queryable),
+      method,
+      new[] { typeof(T), prop.Type },
+      source.Expression,
+      Expression.Quote(lambda));
 
-    var mce = Expression.Call(typeof(Queryable), method, types, q.Expression, exp);
+    return (IOrderedQueryable<T>)source.Provider.CreateQuery<T>(orderByCall);
+  }
 
-    return q.Provider.CreateQuery<T>(mce);
+  /// <summary>
+  ///   ThenBy extension method for IOrderedQueryable.
+  /// </summary>
+  /// <param name="source"> IOrderedQueryable. </param>
+  /// <param name="propertyName"> Property name. </param>
+  /// <param name="descending"> Boolean indicating descending. </param>
+  /// <typeparam name="T"> Type of IOrderedQueryable. </typeparam>
+  /// <returns> IOrderedQueryable (Then BY). </returns>
+  public static IOrderedQueryable<T> ThenBy<T>(
+    this IOrderedQueryable<T> source, string propertyName, bool descending = false)
+  {
+    var param = Expression.Parameter(typeof(T), "p");
+    var prop = Expression.Property(param, propertyName);
+    var lambda = Expression.Lambda(prop, param);
+
+    var method = descending ? "ThenByDescending" : "ThenBy";
+    var orderByCall = Expression.Call(
+      typeof(Queryable),
+      method,
+      new[] { typeof(T), prop.Type },
+      source.Expression,
+      Expression.Quote(lambda));
+
+    return (IOrderedQueryable<T>)source.Provider.CreateQuery<T>(orderByCall);
   }
 }
