68ba1d82dbf16da533c484722adc4dc3eea5638f
Guilherme Potenciano <gpotenciano@topconsuite.com>
2024-02-05 11:42:20 -0300

    feat(backend): create migration to add composite key in sample

---
+++ Telluria.Utils.Crud.Sample/Migrations/20240205144016_AddCompositeKey.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Telluria.Utils.Crud.Sample.Migrations
+{
+    public partial class AddCompositeKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Product",
+                table: "Product");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Product",
+                table: "Product",
+                columns: new[] { "Id", "TenantId" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Product",
+                table: "Product");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Product",
+                table: "Product",
+                column: "Id");
+        }
+    }
+}
