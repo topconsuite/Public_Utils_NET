d60d99e9692fdcc927b6298b20f37b35a98fc55f
Dirceu Wilian de Camargo Conte <wilianconte@gmail.com>
2024-04-27 16:48:23 -0300

    Impruvements in transaction service

--- Telluria.Utils.Crud/Services/ITransactionService.cs
+++ Telluria.Utils.Crud/Services/ITransactionService.cs
@@ -1,14 +1,13 @@
 using System;
 using System.Threading.Tasks;
 using System.Transactions;
-using Telluria.Utils.Crud.Entities;
 
 namespace Telluria.Utils.Crud.Services;
 
 public interface ITransactionService
 {
   bool HasTransaction { get; set; }
-  void AddChange(IntegrationMessage message);
+  event TransactionService.TransactionEventHandlerAsync TransactionCompletedAsync;
   Task ExecuteTransactionAsync(Func<Task> transactionalOperation, TransactionScopeAsyncFlowOption option = TransactionScopeAsyncFlowOption.Enabled);
   Task<T> ExecuteTransactionAsync<T>(Func<Task<T>> transactionalOperation, TransactionScopeAsyncFlowOption option = TransactionScopeAsyncFlowOption.Enabled);
 }
