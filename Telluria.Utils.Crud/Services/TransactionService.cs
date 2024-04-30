310c4bcc9ef26d7289d67952cd445a3b8758157c
Dirceu Wilian de Camargo Conte <wilianconte@gmail.com>
2024-04-30 10:07:26 -0300

    fixing transaction service to reuse the same transaction when a process uses a transaction inner the other transaction

--- Telluria.Utils.Crud/Services/TransactionService.cs
+++ Telluria.Utils.Crud/Services/TransactionService.cs
@@ -30,64 +30,73 @@ public class TransactionService : ITransactionService
 
   public async Task ExecuteTransactionAsync(Func<Task> transactionalOperation, TransactionScopeAsyncFlowOption option = TransactionScopeAsyncFlowOption.Enabled)
   {
-    if (HasTransaction)
-      throw new Exception("Não é permitido o encadeamento de transações!");
-
-    HasTransaction = true;
-
-    var transactionOptions = new TransactionOptions
+    if (!HasTransaction)
     {
-      Timeout = TimeSpan.FromHours(1)
-    };
+      HasTransaction = true;
 
-    try
-    {
-      using (var scope = new TransactionScope(TransactionScopeOption.RequiresNew, transactionOptions, option))
+      var transactionOptions = new TransactionOptions
       {
+        Timeout = TimeSpan.FromHours(1)
+      };
+
+      try
+      {
+        using var scope = new TransactionScope(TransactionScopeOption.RequiresNew, transactionOptions, option);
+
         // Execute the transactional operations
         await transactionalOperation();
 
         // If all operations were successful, complete the transaction
         scope.Complete();
       }
-    }
-    finally
-    {
-      HasTransaction = false;
+      finally
+      {
+        HasTransaction = false;
 
-      // Prepara mensagens
-      await OnTransactionCompleted();
+        // Prepara mensagens
+        await OnTransactionCompleted();
+      }
+    }
+    else
+    {
+      // Execute the transactional operations
+      await transactionalOperation();
     }
   }
 
   public async Task<T> ExecuteTransactionAsync<T>(Func<Task<T>> transactionalOperation, TransactionScopeAsyncFlowOption option = TransactionScopeAsyncFlowOption.Enabled)
   {
-    if (HasTransaction)
-      throw new Exception("Não é permitido o encadeamento de transações!");
-
-    HasTransaction = true;
-
-    T result;
-
-    try
+    if (!HasTransaction)
     {
-      using (var scope = new TransactionScope(TransactionScopeOption.RequiresNew, option))
+      HasTransaction = true;
+
+      T result;
+
+      try
       {
-        // Execute the transactional operations
-        result = await transactionalOperation();
+        using (var scope = new TransactionScope(TransactionScopeOption.RequiresNew, option))
+        {
+          // Execute the transactional operations
+          result = await transactionalOperation();
 
-        // If all operations were successful, complete the transaction
-        scope.Complete();
+          // If all operations were successful, complete the transaction
+          scope.Complete();
+        }
       }
+      finally
+      {
+        HasTransaction = false;
+
+        // Prepara mensagens
+        await OnTransactionCompleted();
+      }
+
+      return result;
     }
-    finally
+    else
     {
-      HasTransaction = false;
-
-      // Prepara mensagens
-      await OnTransactionCompleted();
+      // Execute the transactional operations
+      return await transactionalOperation();
     }
-
-    return result;
   }
 }
