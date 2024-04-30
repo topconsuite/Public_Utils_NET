17d214b4371f42ae8dfbe2d926aa47ca4d5351e4
Dirceu Wilian de Camargo Conte <wilianconte@gmail.com>
2024-04-30 10:23:00 -0300

    adding method to messageService

--- Telluria.Utils.Crud/Services/IMessageBrokerService.cs
+++ Telluria.Utils.Crud/Services/IMessageBrokerService.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Telluria.Utils.Crud.Entities;
 
@@ -6,5 +7,7 @@ namespace Topcon.Tech.Domain.Interfaces.Services;
 public interface IMessageBrokerService
 {
   Task SendMessageAsync(string queueOrTopicName, string message);
-  Task SendIntegrationMessageAsync(IntegrationMessage content);
+
+  Task SendIntegrationMessageAsync(IntegrationMessage integrationMessage);
+  Task SendIntegrationMessageAsync(List<IntegrationMessage> integrationMessages);
 }
